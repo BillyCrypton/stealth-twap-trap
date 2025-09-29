8d86d5c53ee6ed58cf4ad741d867dfc85e54dbeb
BillyCrypton <240mbbs12@gmail.com>
2025-09-29 22:12:11 +0100

    Update MockERC20.sol

--- src/mocks/MockERC20.sol
+++ src/mocks/MockERC20.sol
@@ -5,9 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
-        _mint(msg.sender, 1e24); // mint 1M tokens to deployer
+        _mint(msg.sender, 1_000_000 * (10 ** decimals())); // mint to deployer
     }
 
+    // convenient mint for tests
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
